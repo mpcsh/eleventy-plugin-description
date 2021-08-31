3cbabaeb5e160a26d83316ceee1dae937f709e28
Mark Cohen <git@mail.mpc.sh>
2021-08-31 00:35:50 -0400

    Make description function more robust; testable in REPL

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
-const htmlToText = require('html-to-text');
+const { htmlToText } = require("html-to-text");
+
+function description(
+  templateContent,
+  overrides = {
+    htmlToTextOverrides: {},
+    phraseRegex: /(\p{Terminal_Punctuation}\p{White_Space})/gu,
+    lengthCutoff: 200,
+    terminator: "...",
+  }
+) {
+  let content = htmlToText(templateContent, {
+    wordwrap: false,
+    ignoreHref: true,
+    ignoreImage: true,
+    uppercaseHeadings: false,
+    ...overrides.htmlToTextOverrides,
+  });
+
+  let phrases = content.split(overrides.phraseRegex);
+
+  let description = "";
+  while (phrases.length > 0 && description.length < overrides.lengthCutoff) {
+    description += phrases.shift();
+  }
+  description += overrides.terminator;
+
+  return description;
+}
 
 module.exports = {
+  description,
   configFunction(
     eleventyConfig,
     overrides = {
       htmlToTextOverrides: {},
       phraseRegex: /(\p{Terminal_Punctuation}\p{White_Space})/gu,
       lengthCutoff: 200,
-      terminator: '...',
-    },
+      terminator: "...",
+    }
   ) {
-    eleventyConfig.addFilter('description', templateContent => {
-      let content = htmlToText.fromString(templateContent, {
-        wordwrap: false,
-        ignoreHref: true,
-        ignoreImage: true,
-        uppercaseHeadings: false,
-        ...overrides.htmlToTextOverrides,
-      });
-
-      let phrases = content.split(overrides.phraseRegex);
-
-      let description = phrases.shift();
-      while (description.length < overrides.lengthCutoff) {
-        description += phrases.shift() + phrases.shift();
-      }
-
-      description += overrides.terminator;
-      return description;
-    });
+    eleventyConfig.addFilter("description", (text) =>
+      description(text, overrides)
+    );
   },
 };
